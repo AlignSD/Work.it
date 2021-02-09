2b08811d252b7b5ad47f85b52dae1e2fe1fb9328
Evan Alioto <EvanJAlioto@gmail.com>
2021-02-08 19:50:53 -0800

    setup mongoDB atlas

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require("express");
 const mongoose = require("mongoose");
 const logger = require("morgan");
 
+
+require('dotenv').config()
+
 const PORT = process.env.PORT || 3000;
 
 const db = require('./models');
