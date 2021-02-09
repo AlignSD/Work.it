6a52a517abbb06d4f84d01dccd8ae860860c5fc2
Evan Alioto <EvanJAlioto@gmail.com>
2021-02-08 19:56:09 -0800

    rearranged html routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -5,13 +5,7 @@ const router = require("express").Router();
 // set path to public folder for easier to use filepaths
 let publicPath = path.join(__dirname, '../public');
 
-
-    // GET index.html
-    router.get("/", (req, res) => {
-        res.sendFile('/index.html', { root: publicPath });
-    })
-
-    // GET stats.html
+    git // GET stats.html
     router.get("/stats", (req, res) => {
         res.sendFile('/stats.html', { root: publicPath });
     })
@@ -19,6 +13,11 @@ let publicPath = path.join(__dirname, '../public');
     // GET excercise.html
     router.get("/exercise", (req, res) => {
         res.sendFile('/exercise.html', { root: publicPath });
+    })
+
+     // GET index.html
+     router.get("/", (req, res) => {
+        res.sendFile('/index.html', { root: publicPath });
     })
     
     // export router module
